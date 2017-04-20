ecd33c3be99620771523d31824f22e96a626630e
Nicolae Cindea <ncindea@gmail.com>
2017-04-20 16:10:34 +0200

    We fixe some bugs and we add a test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using TimeSchemes
 using Base.Test
-
-# write your own tests here
-@test 1 == 2
+##
+M = 1
+B = 1
+K = 1
+v0 = 1
+v1 = 0
+t = linspace(0, 1, 1001)
+F = zeros(t)
+v = TimeSchemes.newmark(M, B, K, F, v0, v1, t)
