4b334c2c85eacd1e85e9ce9b14c1394c71d2851e
Nicolae Cindea <ncindea@gmail.com>
2017-05-20 09:29:06 +0200

    Modification of the Newmark function and testing.

--- src/TimeSchemes.jl
+++ src/TimeSchemes.jl
@@ -1,17 +1,15 @@
 module TimeSchemes
 
-"
-    function newmark(M, B, K, F, v0, v1, t)
-    Solve a second order in time system of ODES using a Newmark discretization scheme.
+"""
+  function newmark(M, B, K, F, v0, v1, t; β = 0.25, γ = 0.5, outputNodes = [])
 
-    The form of the system is as follows:
-    M * v''(t) + B * v'(t) + K * v(t) = F(t)
+Solve a second order in time system of ODES using a Newmark discretization scheme.
+
+The form of the system is as follows:
     v(0) = v_0,   v'(0) = v_1
-"
-function newmark(M, B, K, F, v0, v1, t)
+"""
+function newmark(M, B, K, F, v0, v1, t; β = 0.25, γ = 0.5, outputNodes = [])
   # β and γ are the parameters of the scheme
-  β = 0.25
-  γ = 0.5
 
 
   N = length(v0) # number of equations in the system
@@ -19,28 +17,36 @@ function newmark(M, B, K, F, v0, v1, t)
 
   # ASSUMPTION : t is a equidistributed vector
   dt = t[2] - t[1]
-
-  V = zeros(2 * N, T) # solution to return
+  V = []
+  if length(outputNodes) != 0
+    V = zeros(length(outputNodes), T)
+  else
+    V = zeros(2 * N, T) # solution to return
+    outputNodes = 1:(2*N)
+  end
 
   p = 1:N               # first N components of the solution v
   q = (N + 1):(2 * N)   # last  N components of the solution v'
 
+  cV = zeros(2 * N, 1)
   # initialization with initial contion
-  V[p, 1] = v0                       # initial position
-  V[q, 1] = v1                       # initial velocity
+  cV[p] = v0                       # initial position
+  cV[q] = v1                       # initial velocity
   a = M \ (F[:,1] - B * v1 - K * v0) # initial acceleration
+  V[:, 1] = cV[outputNodes]
 
   MI = M + γ * dt * B + β * dt^2 * K
 
   # for details about this scheme see [Hughes, p. 490]
   for n = 2:T
-    d = V[p, n - 1] + dt * V[q, n - 1] + dt^2 / 2 * (1 - 2 * β) * a
-    v = V[q, n - 1] + (1 - γ) * dt * a
+    d = cV[p] + dt * cV[q] + dt^2 / 2 * (1 - 2 * β) * a
+    v = cV[q] + (1 - γ) * dt * a
     a = MI \ (F[:, n] - B * v - K * d)
-    V[p, n] = d + β * dt^2 * a
-    V[q, n] = v + γ * dt * a
+    cV[p] = d + β * dt^2 * a
+    cV[q] = v + γ * dt * a
+    V[:, n] = cV[outputNodes]
   end
-  V
+  V, cV
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,10 @@ v0 = 1
 v1 = 0
 t = linspace(0, 1, 10001)'
 F = zeros(t)
-v = TimeSchemes.newmark(M, B, K, F, v0, v1, t)
+v, cV = TimeSchemes.newmark(M, B, K, F, v0, v1, t)
 
 sol = 1 / 3 * exp(- t / 2) .* (sqrt(3) * sin(sqrt(3) / 2 * t) + 3  * cos(sqrt(3) / 2 * t))
-@test maximum(abs(v[1, :]' - sol)) < 1e-9
+sold = - 1 / 6 * exp(- t / 2) .* (sqrt(3) * sin(sqrt(3) / 2 * t) + 3  * cos(sqrt(3) / 2 * t)) +
+    1 / 3 * exp(- t / 2) .* (3 / 2 * cos(sqrt(3) / 2 * t) - 3 * sqrt(3) / 2 * sin(sqrt(3) / 2 * t))
+@test (maximum(abs(v[1, :]' - sol)) < 1e-9) && (maximum(abs(v[2, :]' - sold)) < 1e-9)
+println("All tests passed.")
